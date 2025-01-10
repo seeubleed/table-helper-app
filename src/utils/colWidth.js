082928c71e9ca413a91abf04ae444b391cda59eb
seeubleed <therealveryrare@gmail.com>
2025-01-11 00:17:47 +0500

    update

--- src/utils/colWidth.js
+++ src/utils/colWidth.js
@@ -11,7 +11,6 @@ function setStaticColWidth(worksheet, width = 20) {
 function setCustomColWidth(worksheet, columnWidths) {
   if (!worksheet) throw new Error('Лист не определён.')
 
-  // columnWidths — объект вида { 1: 20, 2: 20, 3: 45, 4: 45 }
   for (const [colIndex, width] of Object.entries(columnWidths)) {
     const column = worksheet.getColumn(parseInt(colIndex, 10))
     column.width = width
