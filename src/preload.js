6d6c4bc380155589092f39c738c0cf7d4dbb8624
seeubleed <therealveryrare@gmail.com>
2025-01-11 13:02:05 +0500

    optimization update

--- src/preload.js
+++ src/preload.js
@@ -1,23 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron')
-// const Store = require('electron-store')
 
 contextBridge.exposeInMainWorld('electronAPI', {
   selectFile: () => ipcRenderer.invoke('select-file'),
-  processFile: (
-    filePath,
-    toggleColumnCorrect,
-    toggleColumnComment,
-    toggleHiglight,
-    toggleHighlightCorrect
-  ) =>
-    ipcRenderer.invoke(
-      'process-file',
-      filePath,
-      toggleColumnCorrect,
-      toggleColumnComment,
-      toggleHiglight,
-      toggleHighlightCorrect
-    ),
+  processFile: (filePath, toggleColumnCorrect, toggleColumnComment, toggleHiglight, toggleHighlightCorrect) => ipcRenderer.invoke('process-file', filePath, toggleColumnCorrect, toggleColumnComment, toggleHiglight, toggleHighlightCorrect),
   saveFile: tempFilePath => ipcRenderer.invoke('save-file', tempFilePath),
   minimizeWindow: () => ipcRenderer.send('window-minimize'),
   closeWindow: () => ipcRenderer.send('window-close'),
